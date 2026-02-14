4e6988889ad4e4e46a5f4dc5ec14d561ec356f61
Carlos Benitez <110142022+carlobeni@users.noreply.github.com>
2026-02-13 23:33:34 -0300

    Add comment on JAX automatic differentiation
    Added a comment explaining automatic differentiation in JAX.

--- RUTINA6.py
+++ RUTINA6.py
@@ -1,3 +1,5 @@
+#df = grad(f) # jax reconoce automaticamente que debe derivar respecto al parámetro de f definido previamente Ej: def f(r) # deriva respecto a r
+
 ################RUTINA006##########################
 for i in range(n):
  p=p0-f(p0)/df(p0)
