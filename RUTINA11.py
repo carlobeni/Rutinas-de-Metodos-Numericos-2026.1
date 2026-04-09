a3b0ee5a7f35c660e95a7570c227a3ce06a96842
Carlos Benitez <110142022+carlobeni@users.noreply.github.com>
2026-04-09 12:09:47 -0300

    Refactor polynomial interpolation to use Vandermonde
    Removed old polynomial interpolation code and added Vandermonde matrix solution.

--- RUTINA11.py
+++ RUTINA11.py
@@ -1,15 +1,6 @@
-n=len(x)
-p=np.poly1d([1,-x[0]])
-P=np.poly1d([y[0]])
-for i in range(1,n):
- a=(y[i]-P(x[i]))/p(x[i])
- P=np.polyadd(P,a*p)
- p=np.polymul(p,np.poly1d([1,-x[i]]))
-#print(np.array((P),dtype=float))
-print(P)
-
-
-
+################RUTINA011##########################
+v=np.vander(x,N=m,increasing=True)
+a=np.linalg.solve(v,y)
 
 
 
