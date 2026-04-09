af1d4586a3180b33a1014181bbcc7bb54d64a5ce
Carlos Benitez <110142022+carlobeni@users.noreply.github.com>
2026-04-09 12:10:10 -0300

    Update RUTINA12.py

--- RUTINA12.py
+++ RUTINA12.py
@@ -1,5 +1,7 @@
-A = 0.0
-h = (b - a) / n
-for xi in np.arange(a, b - 2*h, 2*h):
-    A = A + (h/3) * (f(xi) + 4*f(xi + h) + f(xi + 2*h))
-print(A)
+################RUTINA012##########################
+for i in range(n):
+ a=np.delete(np.arange(n),i)
+ p=np.poly1d([1,-x[a[0]]])
+ for j in range(1,n-1):
+  p=np.polymul(p,np.poly1d([1,-x[a[j]]]))
+ P+=y[i]*p/p(x[i])
