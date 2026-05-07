c32b97161adefb4a85d6f4c9e201259705b3b46e
Carlos Benitez <110142022+carlobeni@users.noreply.github.com>
2026-05-07 19:26:23 -0300

    Add implementation in RUTINA25.py
    Implement numerical method for solving differential equations using Runge-Kutta technique.

--- RUTINA25.py
+++ RUTINA25.py
@@ -1,1 +1,33 @@
+################RUTINA025##########################
+m=len(y0)
+a=np.hstack((np.zeros((m-1,1)),np.eye((m-1))))
+A=lambda x:np.vstack((a,np.array(([...]),dtype=float)))
+B=lambda x:np.vstack((np.zeros((m-1,1)),np.array(([...]),dtype=float)))
+x1=x0+h
+x2=x1+h
+x3=x2+h
+for i in range(n):
+    dy1=A(x1)@y1+B(x1)
+    dy2=A(x2)@y2+B(x2)
+    dy3=A(x3)@y3+B(x3)
+    
+    p=y0+(4/3)*h*(2*dy1-dy2+2*dy3)
 
+    if i>0:
+        mod=p+(28/29)*(y3-p0)
+    else: 
+        mod=p
+
+    x4=x3+h
+    dy4=A(x4)@mod+B(x4)
+    
+    y=y2+(h/3)*(dy2+4*dy3+dy4)
+    
+    y0=y1.copy()
+    y1=y2.copy()
+    y2=y3.copy()
+    y3=y.copy()
+    x1=x2
+    x2=x3
+    x3=x4
+    p0=p.copy()
